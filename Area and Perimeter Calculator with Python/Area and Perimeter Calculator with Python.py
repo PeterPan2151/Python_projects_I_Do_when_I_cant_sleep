0d3762b1414afd8dccf818738ece07f285310e5a
Salvador Montiel <126306642+PeterPan2151@users.noreply.github.com>
2025-04-05 21:47:02 -0600

    Add files via upload

---
+++ Area and Perimeter Calculator with Python/Area and Perimeter Calculator with Python.py
@@ -0,0 +1,10 @@
+def rectangle_area(width, height):
+    return width * height
+
+
+def rectangle_perimeter(width, height):
+    return (width * 2) + (height * 2)
+
+
+print(rectangle_area(10, 3))
+print(rectangle_perimeter(10, 3))
